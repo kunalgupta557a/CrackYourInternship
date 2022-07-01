b96a46ba5490dd67efc1f59a55ff883ebc503f85
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-07-01 16:13:23 +0530

    Time: 73 ms (52.17%), Space: 17 MB (34.85%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,15 +1,17 @@
 
 
-
+//https://www.geeksforgeeks.org/sorting-2d-vector-in-c-set-1-by-row-and-column/
 class Solution {
 public:
     static bool sortcol(const vector<int>& v1, const vector<int>& v2)
 {
     return v1[1] > v2[1];    // > this is neccasary here.
+        // v1[1] & v2[1] are elements of 2 coloumn 
+        // we are returning v1[1] only if its greater than v2[1] so that how it get sorted
 }
   //sortcol wont work if it isn't static
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),boxTypes.end(),sortcol);
+        sort(boxTypes.begin(),boxTypes.end(),sortcol); // 3 parameter suggest how is the array sorted
         int ans=0;
         for(auto box : boxTypes){
             int x= min(box[0],truckSize);
