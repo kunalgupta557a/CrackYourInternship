83ea9ea8aa2c1b25e1dfb34a768746a78f2e4649
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-07-01 16:03:06 +0530

    Time: 59 ms (67.01%), Space: 17 MB (34.85%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     static bool sortcol(const vector<int>& v1, const vector<int>& v2)
 {
-    return v1[1] > v2[1];
+    return v1[1] > v2[1];    // > this is neccasary here.
 }
   //sortcol wont work if it isn't static
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
