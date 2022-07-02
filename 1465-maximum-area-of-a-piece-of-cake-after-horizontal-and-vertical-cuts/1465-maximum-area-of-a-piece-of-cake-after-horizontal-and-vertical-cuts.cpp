1b174dfdd9e5fd5dea049c6cfd4bb1aae08082d7
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-07-02 22:07:53 +0530

    Time: 115 ms (35.31%), Space: 32.3 MB (76.96%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& hor, vector<int>& ver) {
+        sort(hor.begin(),hor.end());
+        sort(ver.begin(),ver.end());
+        long long int hdiff=hor[0],vdiff=ver[0];
+        //diff from 0 and last
+        const unsigned int M = 1000000007;
+        for(int i=1;i<hor.size();i++){
+            if((hor[i]-hor[i-1])>hdiff) hdiff=abs(hor[i]-hor[i-1]);    
+        }
+        
+        for(int i=1;i<ver.size();i++){
+            if((ver[i]-ver[i-1])>vdiff) vdiff=abs(ver[i]-ver[i-1]);
+            }
+        
+        if(w-ver[ver.size()-1]>vdiff) vdiff=w-ver[ver.size()-1];
+        
+        if(h-hor[hor.size()-1]>hdiff) hdiff=h-hor[hor.size()-1];
+        
+       long long int x= (hdiff * vdiff) %M; 
+        return x;
+    }
+};
+    
+    
