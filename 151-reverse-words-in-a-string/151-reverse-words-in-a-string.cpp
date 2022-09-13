87fef6328076891d32765e9cc195b19e944b8305
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-09-13 21:54:21 +0530

    Time: 44 ms (5.61%), Space: 7.1 MB (91.43%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -4,7 +4,7 @@ public:
         
     //trim trailing and ending 0
        while(s[0]==' ') s.erase(s.begin());
-        while(s[s.size()-1]==' ') s.erase(s.end()-1);
+        while(s[s.size()-1]==' ') s.erase(s.end()-1); // this erase last element not s.erase()-1;
         
         reverse(s.begin(),s.end());
         int beg=0;
@@ -12,10 +12,10 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]==' '){
                 if(s[i+1]==' '){
-                    s.erase(s.begin()+i+1);
+                    s.erase(s.begin()+i+1); //remove all multiple spaces first
                     i--;
                    continue;}
-                reverse(s.begin()+beg,s.begin()+i);
+                reverse(s.begin()+beg,s.begin()+i); // mistake here as s.begin()+i-1
                 beg=i+1;
             }   
             if(i==s.size()-1) reverse(s.begin()+beg,s.end());
