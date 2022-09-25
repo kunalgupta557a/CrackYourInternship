3d34d8318260a8e15363b5b50539886581553f4d
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-09-25 15:26:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.31%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,10 +4,12 @@ public:
         vector<vector<int>> ans;
         vector<int> level;
         int x=0;
+        
         while(x<num){
         for(int i=x-1;i>0;i--){
             
             level[i]=level[i]+level[i-1];
+            
         }   
             x++;
             level.push_back(1);
