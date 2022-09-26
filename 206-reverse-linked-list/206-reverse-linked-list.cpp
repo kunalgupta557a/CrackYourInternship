325852bc35437f74330f8939453d1755c5345a19
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-09-26 12:47:33 +0530

    Time: 7 ms (83.27%), Space: 8.3 MB (79.02%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,15 +11,15 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL; // a way to make ptr which pts at node.. it is not a node it is a ptr
-        ListNode* curr = head;
-        ListNode* nextptr = NULL;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nextptr=NULL;
         while(curr){
-            nextptr = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextptr;
+            nextptr=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextptr;
         }
-        return prev; // here prev is pointing to head so we are returning it as when giving linked list the head was given inside reverselist fn.
+        return prev;
     }
 };
