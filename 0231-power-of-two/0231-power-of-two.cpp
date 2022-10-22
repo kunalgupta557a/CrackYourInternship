1b24d0c9e39b7151fb2c80e86ea672730ce89d6d
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-10-23 00:10:22 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.29%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n>0 && (n&(n-1))==0) return true;
-        return false;
+        if(n==0) return false;
+        return(n==1 || (n%2==0 && isPowerOfTwo(n/2)));
     }
 };
 /*
 
-
+if(n>0 && (n&(n-1))==0) return true;
+        return false;
 
 */
