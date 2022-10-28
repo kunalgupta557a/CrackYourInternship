a5aa136dfe789c5a9f6ec269eb9b94e9985237d6
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-10-28 22:35:23 +0530

    Time: 641 ms (12.60%), Space: 115.1 MB (17.99%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        map<int,int> m{{0,0}}; // inserting 0,0 is important 
+        map<int,int> m{{0,0}}; // inserting 0,0 is important as remainder becomes 0 at starting then also maybe we found ans so to repeat the remainder we need this
         int sum=0;
        //use of remainder property
         for(int i=0;i<nums.size();i++){
