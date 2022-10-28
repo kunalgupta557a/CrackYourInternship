8f7bf2f17119c56f329d59faca60506c866656fb
kunalgupta557a <67480044+kunalgupta557a@users.noreply.github.com>
2022-10-28 22:34:27 +0530

    Time: 669 ms (9.36%), Space: 115.1 MB (17.99%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        map<int,int> m{{0,0}}; // inserting 0,0 is important
+        map<int,int> m{{0,0}}; // inserting 0,0 is important 
         int sum=0;
-       
+       //use of remainder property
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
+            
             if(!m.count(sum%k)) m[sum%k]=i+1; 
             else{
                 if(m[sum%k]<i) return true;
             }
-            
+            //using i and i+1 to know atleast two element in between come
         }
         return false;
         
